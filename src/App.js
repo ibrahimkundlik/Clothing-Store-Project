fa2497e941aec2ff4505fccbfb2eb965915fb273
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-03-02 10:23:18 +0530

    Added Firebase OAuth and redirect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
 import React from "react";
 import HomePage from "./pages/homepage/HomePage.component";
 import ShopPage from "./pages/shoppage/ShopPage.component";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import Header from "./components/header/Header.component";
 import "./App.scss";
 import LoginPage from "./pages/loginpage/LoginPage.component";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-	return (
-		<div className="main-container">
-			<Header />
-			<Route exact path="/" component={HomePage} />
-			<Route exact path="/shop" component={ShopPage} />
-			<Route exact path="/login" component={LoginPage} />
-		</div>
-	);
+class App extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			currentUser: null,
+			isLoading: true,
+		};
+	}
+
+	unsubscribeFromAuth = null;
+	// setting oauth
+	componentDidMount() {
+		this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+			this.setState({ currentUser: user, isLoading: false });
+		});
+	}
+	// removing oauth
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
+
+	render() {
+		return (
+			<div className="main-container">
+				<Header
+					currentUser={this.state.currentUser}
+					isLoading={this.state.isLoading}
+				/>
+				<Route exact path="/" component={HomePage} />
+				<Route exact path="/shop" component={ShopPage} />
+				{this.state.currentUser ? (
+					<Redirect to="/" />
+				) : (
+					<Route exact path="/login" component={LoginPage} />
+				)}
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { Link } from "react-router-dom";
 import "./Header.styles.scss";
+import { auth } from "../../firebase/firebase.utils";
 
-const Header = () => {
+const Header = ({ currentUser, isLoading }) => {
 	return (
 		<header className="header">
 			<div className="logo-cont">
@@ -12,8 +13,18 @@ const Header = () => {
 				</Link>
 			</div>
 			<nav className="nav-links">
-				<Link to="/shop">SHOP</Link>
-				<Link to="/login">LOGIN</Link>
+				<Link className="nav-link" to="/shop">
+					SHOP
+				</Link>
+				{currentUser ? (
+					<button className="nav-link" onClick={() => auth.signOut()}>
+						SIGN OUT
+					</button>
+				) : isLoading ? null : (
+					<Link to="/login" className="nav-link">
+						LOGIN
+					</Link>
+				)}
 			</nav>
 		</header>
 	);
