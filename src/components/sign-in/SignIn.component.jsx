2158565833f525a23d42a725584b5fa975b7f9d8
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-04-09 23:28:12 +0530

    Added react hooks for SignIn component

--- src/components/sign-in/SignIn.component.jsx
+++ src/components/sign-in/SignIn.component.jsx
@@ -1,74 +1,65 @@
 //react
-import React from "react";
+import React, { useState } from "react";
 //components
 import FormInput from "../form-input/FormInput.component";
 import CustomBtn from "../custom-btn/CustomBtn.component";
 //styled-components
 import { SignInContainer } from "./SignIn.styles";
+//redux
+import { selectErrorState } from "../../redux/user/user.selector";
+import { createStructuredSelector } from "reselect";
+import { connect } from "react-redux";
 //redux-saga
 import { googleSignInStart, epSignInStart } from "../../redux/user/user.action";
-import { connect } from "react-redux";
 
-class SignIn extends React.Component {
-	constructor() {
-		super();
+const SignIn = ({ googleSignInStart, epSignInStart, signinError }) => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState(false);
 
-		this.state = {
-			email: "",
-			password: "",
-			error: false,
-		};
-	}
-
-	handleSubmit = async (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-		const { email, password } = this.state;
-		const { epSignInStart } = this.props;
 		epSignInStart(email, password);
 	};
 
-	handleChange = (e) => {
-		const { value, name } = e.target;
-		this.setState({ [name]: value });
-	};
+	return (
+		<SignInContainer>
+			<h2>I already have an account</h2>
+			<p>Sign in with your email and password</p>
+			{error ? (
+				<p className="signin-error">
+					We couldn't sign you in. Email or Password is incorrect. ⚠️
+				</p>
+			) : null}
+			<form onSubmit={handleSubmit} autoComplete="off">
+				<FormInput
+					type="email"
+					name="email"
+					label="Email"
+					id="sign-in-email"
+					onChange={(e) => setEmail(e.target.value)}
+					required
+				/>
+				<FormInput
+					type="password"
+					name="password"
+					label="Password"
+					id="sign-in-password"
+					onChange={(e) => setPassword(e.target.value)}
+					required
+				/>
+				<CustomBtn type="submit"> SIGN IN </CustomBtn>
+				<CustomBtn type="button" googleAuth onClick={googleSignInStart}>
+					SIGN IN WITH GOOGLE
+				</CustomBtn>
+			</form>
+		</SignInContainer>
+	);
+};
 
-	render() {
-		const { googleSignInStart } = this.props;
-		return (
-			<SignInContainer>
-				<h2>I already have an account</h2>
-				<p>Sign in with your email and password</p>
-				{this.state.error ? (
-					<p className="signin-error">
-						We couldn't sign you in. Email or Password is incorrect. ⚠️
-					</p>
-				) : null}
-				<form onSubmit={this.handleSubmit} autoComplete="off">
-					<FormInput
-						type="email"
-						name="email"
-						label="Email"
-						id="sign-in-email"
-						onChange={this.handleChange}
-						required
-					/>
-					<FormInput
-						type="password"
-						name="password"
-						label="Password"
-						id="sign-in-password"
-						onChange={this.handleChange}
-						required
-					/>
-					<CustomBtn type="submit"> SIGN IN </CustomBtn>
-					<CustomBtn type="button" googleAuth onClick={googleSignInStart}>
-						SIGN IN WITH GOOGLE
-					</CustomBtn>
-				</form>
-			</SignInContainer>
-		);
-	}
-}
+const mapStateToProps = createStructuredSelector({
+	signinError: selectErrorState,
+});
 
 const mapDispatchToProps = (dispatch) => ({
 	googleSignInStart: () => dispatch(googleSignInStart()),
@@ -76,4 +67,4 @@ const mapDispatchToProps = (dispatch) => ({
 		dispatch(epSignInStart({ email, password })),
 });
 
-export default connect(null, mapDispatchToProps)(SignIn);
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
