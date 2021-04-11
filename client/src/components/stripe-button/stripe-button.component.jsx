a0e8cecff13d80c181f5df32611371d6b7bbe781
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-04-11 15:28:55 +0530

    Setting up Stripe backend for payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -2,15 +2,34 @@
 import React from "react";
 //stripe
 import StripeCheckout from "react-stripe-checkout";
+//axios
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
 	const centPrice = price * 100;
 	const pubKey =
 		"pk_test_51IW2KCKDwFBXv9KbhejkGrrqrFIVkoZ46AAEPQ1JJBL7PHlV7SsBFClo9H2Nibb4JBIph4aZYPiFFxbzJXpkepKK00CNFemUSW";
+
 	const onToken = (token) => {
-		console.log(token);
-		alert("Payment Successful.");
+		axios({
+			url: "payment",
+			method: "post",
+			data: {
+				amount: centPrice,
+				token,
+			},
+		})
+			.then((response) => {
+				alert("PAYMENT SUCCESSFUL");
+			})
+			.catch((error) => {
+				console.log(error);
+				alert(
+					"PAYMENT FAILED. Please make sure to use the provided credit card for payments."
+				);
+			});
 	};
+
 	return (
 		<StripeCheckout
 			label="Pay Now"
@@ -23,7 +42,6 @@ const StripeCheckoutButton = ({ price }) => {
 			panelLabel="Pay Now"
 			token={onToken}
 			stripeKey={pubKey}
-			allowRememberMe={false}
 		/>
 	);
 };
